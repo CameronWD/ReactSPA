3b4876dd997e3e530eb6a63a5fd57c64a9fc7f40
CameronWD <cwilliams.developer@gmail.com>
2023-08-06 23:46:33 +1000

    updated colour scheme

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 
 const NavBar = () => {
   return (
-    <nav class="navbar navbar-expand-lg bg-primary-subtle">
-  <div class="container-fluid">
-    <Link class="navbar-brand" to="/">Journal</Link>
+    <nav class="navbar navbar-expand-lg bg-primary-subtle" data-bs-theme="dark">
+    <div class="container-fluid">
+      <Link class="navbar-brand" to="/">Journal</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
